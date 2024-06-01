7be71425f7bb91c6f75b9a0d0a76d37b53218df1
CalvinJOats <88681685+CalvinJOats@users.noreply.github.com>
2024-06-01 03:12:37 -0600

    Completely Re-made main.cpp. Pulled and modified the tls-client-block.c file from the openSSL examples and modified it to fit the planned RSS reader.

--- main.cpp
+++ main.cpp
@@ -1,191 +1,350 @@
-//
-// Created by Calvin Oats on 4/7/2024.
-//
+/*
+ *  Copyright 2023 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License 2.0 (the "License").  You may not use
+ *  this file except in compliance with the License.  You can obtain a copy
+ *  in the file LICENSE in the source distribution or at
+ *  https://www.openssl.org/source/license.html
+ */
 
-#ifdef _WIN32
-#include <winsock2.h>
-#else
-#include <netdb.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <cstring>
-#include <iostream>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+/*
+ * This file has been modified to be used in a proprietary RSS reader and is only being used for personal use.
+ * The original file can be found at https://github.com/openssl/openssl/blob/master/demos/guide/tls-client-block.c
+ * For the purposes of this program the main function has been converted to a callable function
+ */
 
-SSL* ssl;
+/* Include the appropriate header file for SOCK_STREAM */
+#ifdef _WIN32 /* Windows */
+# include <winsock2.h>
+#else /* Linux/Unix */
+# include <sys/socket.h>
 #endif
 
-int SendPacket(const char* buf) {
-    int len = SSL_write(ssl, buf, (int)strlen(buf));
-    if (len < 0) {
-        int err = SSL_get_error(ssl, len);
-        switch (err) {
-            case SSL_ERROR_WANT_WRITE:
-            case SSL_ERROR_WANT_READ:
-                return 0;
-            case SSL_ERROR_ZERO_RETURN:
-            case SSL_ERROR_SYSCALL:
-            case SSL_ERROR_SSL:
-            default:
-                return -1;
-        }
-    }
-    return len;
-}
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <sstream>
 
-int RecvPacket(char* buf) {
-    int len;
-//    char buf[1000000];
-    do {
-        len = SSL_read(ssl, buf, 100);
-        buf[len] = 0;
-        printf("%s\n", buf);
-//        fprintf(fp, "%s",buf);
-    } while (len > 0);
-    if (len < 0) {
-        int err = SSL_get_error(ssl, len);
-        switch (err) {
-            case SSL_ERROR_WANT_WRITE:
-            case SSL_ERROR_WANT_READ:
-                return 1;
-            case SSL_ERROR_ZERO_RETURN:
-            case SSL_ERROR_SYSCALL:
-            case SSL_ERROR_SSL:
-            default:
-                return -1;
-        }
-    }
-    return len;
-}
-
-void log_ssl()
+/* Helper function to create a BIO connected to the server */
+static BIO *create_socket_bio(const char *hostname, const char *port, int family)
 {
-    unsigned long err;
-    while ((err = ERR_get_error())) {
-        char *str = ERR_error_string(err, nullptr);
-        if (!str)
-            return;
-        printf("%s", str);
-        printf("\n");
-        fflush(stdout);
+    int sock = -1;
+    BIO_ADDRINFO *res;
+    const BIO_ADDRINFO *ai = nullptr;
+    BIO *bio;
+
+    /*
+     * Lookup IP address info for the server.
+     */
+    if (!BIO_lookup_ex(hostname, port, BIO_LOOKUP_CLIENT, family, SOCK_STREAM, 0,
+                       &res))
+        return nullptr;
+
+    /*
+     * Loop through all the possible addresses for the server and find one
+     * we can connect to.
+     */
+    for (ai = res; ai != nullptr; ai = BIO_ADDRINFO_next(ai)) {
+        /*
+         * Create a TCP socket. We could equally use non-OpenSSL calls such
+         * as "socket" here for this and the subsequent connect and close
+         * functions. But for portability reasons and also so that we get
+         * errors on the OpenSSL stack in the event of a failure we use
+         * OpenSSL's versions of these functions.
+         */
+        sock = BIO_socket(BIO_ADDRINFO_family(ai), SOCK_STREAM, 0, 0);
+        if (sock == -1)
+            continue;
+
+        /* Connect the socket to the server's address */
+        if (!BIO_connect(sock, BIO_ADDRINFO_address(ai), BIO_SOCK_NODELAY)) {
+            BIO_closesocket(sock);
+            sock = -1;
+            continue;
+        }
+
+        /* We have a connected socket so break out of the loop */
+        break;
     }
+
+    /* Free the address information resources we allocated earlier */
+    BIO_ADDRINFO_free(res);
+
+    /* If sock is -1 then we've been unable to connect to the server */
+    if (sock == -1)
+        return nullptr;
+
+    /* Create a BIO to wrap the socket */
+    bio = BIO_new(BIO_s_socket());
+    if (bio == nullptr) {
+        BIO_closesocket(sock);
+        return nullptr;
+    }
+
+    /*
+     * Associate the newly created BIO with the underlying socket. By
+     * passing BIO_CLOSE here the socket will be automatically closed when
+     * the BIO is freed. Alternatively you can use BIO_NOCLOSE, in which
+     * case you must close the socket explicitly when it is no longer
+     * needed.
+     */
+    BIO_set_fd(bio, sock, BIO_CLOSE);
+
+    return bio;
 }
 
-//int sendall(int s, const char *buf, int *len) {
-//    int total = 0;        // how many bytes we've sent
-//    int bytesleft = *len; // how many we have left to send
-//    int n;
-//
-//    while(total < *len) {
-//        n = send(s, buf+total, bytesleft, 0);
-//        if (n == -1) { break; }
-//        total += n;
-//        bytesleft -= n;
-//    }
-//
-//    *len = total; // return number actually sent here
-//
-//    return n==-1?-1:0; // return -1 on failure, 0 on success
-//}
+/*
+ * Simple application to send a basic HTTP/1.0 request to a server and
+ * print the response on the screen.
+ */
+int XML_Get (int argc, char *argv[]){
+    SSL_CTX *ctx = nullptr;
+    SSL *ssl = nullptr;
+    BIO *bio = nullptr;
+    int res = EXIT_FAILURE;
+    int ret;
+    const char *request_start = "GET /rss.xml HTTP/1.1\r\nConnection: close\r\nHost: ";
+    const char *request_end = "\r\n\r\n";
+    size_t written, readbytes;
+    char buf[160];
+    char *hostname, *port;
+    int argnext = 1;
+    int ipv6 = 0;
+    std::stringstream ss;
+    std::string output, ctitle, cimage, cdesc, cimg_start, ccombined;
+    std::ofstream comicf;
 
-int main() {
-    int                     _buffer_size = 1000000;
-
-    int                     s, sock, cur_con, ssl_sock, sp, rp;
-//    int                     send_success;
-//    ssize_t                 byte_c;
-    struct addrinfo         hints{};
-    struct addrinfo*        result;
-    char                    buf[_buffer_size];
-//    const char*             header;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_flags = AI_CANONNAME;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-
-/** Get Address and Connect Socket for Address */
-    s = getaddrinfo("xkcd.com", "https", &hints, &result);
-    if(s != 0) {
-        std::cerr << "Failed to get address information" << std::endl;
-        return 1;
-    } else {
-        char ip[INET6_ADDRSTRLEN];
-//        for(addrinfo* addr = result; addr; addr = addr->ai_next) {
-//            std::cout << "Output for each addr info from address: ";
-//            std::cout << inet_ntop(addr->ai_family, &reinterpret_cast<sockaddr_in*>(addr->ai_addr)->sin_addr, ip, sizeof(ip)) << "\n";
-//        }
-        sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-        cur_con = connect(sock, result->ai_addr, result->ai_addrlen);
-        if(cur_con != 0) {
-            std::cerr << "Failed to connect to socket" << std::endl;
-            return 2;
-        }
-
-/** SSL  Socket and Connection */
-//  Set SSL initial variables
-        SSL_library_init();
-        SSLeay_add_ssl_algorithms();
-        SSL_load_error_strings();
-
-//  Set the SSL method
-        const SSL_METHOD *ssl_meth = TLS_client_method();
-        SSL_CTX *ctx = SSL_CTX_new (ssl_meth);
-        ssl = SSL_new (ctx);
-        if (!ssl) {
-            printf("Error creating SSL.\n");
-            log_ssl();
-            return -1;
-        }
-
-//  Set the SSL socket and start the connection
-        ssl_sock = SSL_get_fd(ssl);
-        SSL_set_fd(ssl, sock);
-        int ssl_ret = SSL_connect(ssl);
-        if (ssl_ret <= 0) {
-            int err = SSL_get_error(ssl, ssl_ret);
-            char* err_buf = ERR_error_string(errno, nullptr);
-            int fatal = ERR_FATAL_ERROR(errno);
-            printf("Error creating SSL connection.  err=%d\n"
-                   "%s\n"
-                   "Is fatal: %s\n", err, err_buf, fatal?"True":"False");
-            log_ssl();
-            fflush(stdout);
-            return -1;
-        }
-        printf ("SSL connection using %s\n", SSL_get_cipher (ssl));
-
-//  Send the header request and receive the response
-        const char* request = "GET https://xkcd.com/rss.xml HTTP/1.1\n"
-                              "Host: xkcd.com\n"
-                              "\n";
-        do {
-            sp = SendPacket(request);
-        } while(sp <= 0);
-        do {
-            rp = RecvPacket(buf);
-        } while(rp != 0);
-
-/** HTTP Socket and Connection */
-//        header = "GET /rss.xml HTTP/1.1\n"
-//                 "Host: xkcd.com\n"
-//                 "\n";
-//        send_success = SendPacket(header);
-//        if(send_success == -1) {
-//            return 3;
-//        }
-//
-//        byte_c = recv(sock, buf, sizeof buf, 0);
-//        buf[byte_c] = 0;
-//        if(byte_c == 0) {
-//            std::cerr << "Failed to receive data" << std::endl;
-//            return 4;
-//        }
-        std::cout << buf << std::endl;
+    if (argc < 3) {
+        printf("Usage: tls-client-block [-6]  hostname port\n");
+        goto end;
     }
 
-    freeaddrinfo(result);
+    if (!strncmp(argv[argnext], "-6", 2)) {
+        if (argc < 4) {
+            printf("Usage: tls-client-block [-6]  hostname port\n");
+            goto end;
+        }
+        ipv6 = 1;
+        argnext++;
+    }
+    hostname = argv[argnext++];
+    port = argv[argnext];
+
+    /*
+     * Create an SSL_CTX which we can use to create SSL objects from. We
+     * want an SSL_CTX for creating clients so we use TLS_client_method()
+     * here.
+     */
+    ctx = SSL_CTX_new(TLS_client_method());
+    if (ctx == nullptr) {
+        printf("Failed to create the SSL_CTX\n");
+        goto end;
+    }
+
+    /*
+     * Configure the client to abort the handshake if certificate
+     * verification fails. Virtually all clients should do this unless you
+     * really know what you are doing.
+     */
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, nullptr);
+
+    /* Use the default trusted certificate store */
+    if (!SSL_CTX_set_default_verify_paths(ctx)) {
+        printf("Failed to set the default trusted certificate store\n");
+        goto end;
+    }
+
+    /*
+     * TLSv1.1 or earlier are deprecated by IETF and are generally to be
+     * avoided if possible. We require a minimum TLS version of TLSv1.2.
+     */
+    if (!SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION)) {
+        printf("Failed to set the minimum TLS protocol version\n");
+        goto end;
+    }
+
+    /* Create an SSL object to represent the TLS connection */
+    ssl = SSL_new(ctx);
+    if (ssl == nullptr) {
+        printf("Failed to create the SSL object\n");
+        goto end;
+    }
+
+    /*
+     * Create the underlying transport socket/BIO and associate it with the
+     * connection.
+     */
+    bio = create_socket_bio(hostname, port, ipv6 ? AF_INET6 : AF_INET);
+    if (bio == nullptr) {
+        printf("Failed to create the BIO\n");
+        goto end;
+    }
+    SSL_set_bio(ssl, bio, bio);
+
+    /*
+     * Tell the server during the handshake which hostname we are attempting
+     * to connect to in case the server supports multiple hosts.
+     */
+    if (!SSL_set_tlsext_host_name(ssl, hostname)) {
+        printf("Failed to set the SNI hostname\n");
+        goto end;
+    }
+
+    /*
+     * Ensure we check during certificate verification that the server has
+     * supplied a certificate for the hostname that we were expecting.
+     * Virtually all clients should do this unless you really know what you
+     * are doing.
+     */
+    if (!SSL_set1_host(ssl, hostname)) {
+        printf("Failed to set the certificate verification hostname");
+        goto end;
+    }
+
+    /* Do the handshake with the server */
+    if (SSL_connect(ssl) < 1) {
+        printf("Failed to connect to the server\n");
+        /*
+         * If the failure is due to a verification error we can get more
+         * information about it from SSL_get_verify_result().
+         */
+        if (SSL_get_verify_result(ssl) != X509_V_OK)
+            printf("Verify error: %s\n",
+                   X509_verify_cert_error_string(SSL_get_verify_result(ssl)));
+        goto end;
+    }
+
+    /* Write an HTTP GET request to the peer */
+    if (!SSL_write_ex(ssl, request_start, strlen(request_start), &written)) {
+        printf("Failed to write start of HTTP request\n");
+        goto end;
+    }
+    if (!SSL_write_ex(ssl, hostname, strlen(hostname), &written)) {
+        printf("Failed to write hostname in HTTP request\n");
+        goto end;
+    }
+    if (!SSL_write_ex(ssl, request_end, strlen(request_end), &written)) {
+        printf("Failed to write end of HTTP request\n");
+        goto end;
+    }
+
+    /*
+     * Get up to sizeof(buf) bytes of the response. We keep reading until the
+     * server closes the connection.
+     */
+    while (SSL_read_ex(ssl, buf, sizeof(buf), &readbytes)) {
+        /*
+        * OpenSSL does not guarantee that the returned data is a string or
+        * that it is NUL terminated so we use fwrite() to write the exact
+        * number of bytes that we read. The data could be non-printable or
+        * have NUL characters in the middle of it. For this simple example
+        * we're going to print it to stdout anyway.
+        */
+
+        ss.write((const char*)buf, (std::streamsize)readbytes);  // Copy the page content into a stringstream to later convert to a string
+//        fwrite(buf, 1, readbytes, ss);
+    }
+
+    output = ss.str();  // Convert the stringstream to a string
+
+    ctitle = output.substr(output.find("<item>") + 13, (output.find("</title>") - (output.find("<item>") + 13)));  // Find the title of the comic
+    ctitle = ctitle.substr(0, ctitle.find("</title>")); // Not sure why the first substring did not end. This gets the end of the title
+
+    cimg_start = "<image ";
+    cimage = output.substr(output.find("src=\""),  (output.find("\" title=") - (output.find("src=\""))));   // Find the image link for the comic
+    cimage = cimg_start.append(cimage);
+    cimage = cimage.append("\" ");
+
+    cdesc  = output.substr(output.find("title=\""),  (output.find(" alt=") - (output.find("title=\""))));   // Find the Description of the comic
+    cimage = cimage.append(cdesc);
+    cimage = cimage.append(">");
+
+    ccombined = ctitle.append(cimage);
+
+    std::cout << std::endl << ctitle;
+
+    comicf.open("~/comic.html");
+    comicf << ccombined;
+    comicf.close();
+
+    /* In case the response didn't finish with a newline we add one now */
+    printf("\n");
+
+    /*
+     * Check whether we finished the while loop above normally or as the
+     * result of an error. The 0 argument to SSL_get_error() is the return
+     * code we received from the SSL_read_ex() call. It must be 0 in order
+     * to get here. Normal completion is indicated by SSL_ERROR_ZERO_RETURN.
+     */
+    if (SSL_get_error(ssl, 0) != SSL_ERROR_ZERO_RETURN) {
+        /*
+         * Some error occurred other than a graceful close down by the
+         * peer.
+         */
+        printf ("Failed reading remaining data\n");
+        goto end;
+    }
+
+    /*
+     * The peer already shutdown gracefully (we know this because of the
+     * SSL_ERROR_ZERO_RETURN above). We should do the same back.
+     */
+    ret = SSL_shutdown(ssl);
+    if (ret < 1) {
+        /*
+         * ret < 0 indicates an error. ret == 0 would be unexpected here
+         * because that means "we've sent a close_notify and we're waiting
+         * for one back". But we already know we got one from the peer
+         * because of the SSL_ERROR_ZERO_RETURN above.
+         */
+        printf("Error shutting down\n");
+        goto end;
+    }
+
+    /* Success! */
+    res = EXIT_SUCCESS;
+    end:
+    /*
+     * If something bad happened then we will dump the contents of the
+     * OpenSSL error stack to stderr. There might be some useful diagnostic
+     * information there.
+     */
+    if (res == EXIT_FAILURE)
+        ERR_print_errors_fp(stderr);
+
+    /*
+     * Free the resources we allocated. We do not free the BIO object here
+     * because ownership of it was immediately transferred to the SSL object
+     * via SSL_set_bio(). The BIO will be freed when we free the SSL object.
+     */
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    return res;
+}
+
+int main () {
+    char* get_args[3];
+
+    // Allocate and assign values to http request function
+    get_args[0] = (char*)malloc(sizeof(char)*20);
+    strncpy(get_args[0], "placeholder_command", 20);
+
+    get_args[1] = (char*)malloc(sizeof(char)*10);
+    strncpy(get_args[1], "xkcd.com", 10);
+
+    get_args[2] = (char*)malloc(sizeof(char)*5);
+    strncpy(get_args[2], "443", 5);
+
+    // Call http request function for the specified website
+    XML_Get(3, get_args);
+
+    // Clear allocated memory from the http request function arguments
+    for(auto i : get_args) {
+        free(i);
+    }
+
     return 0;
 }
